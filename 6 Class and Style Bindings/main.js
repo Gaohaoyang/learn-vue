1140366193faf757467b70be20acddc191ada329
Gaohaoyang <gaohaoyang126@126.com>
2016-05-13 22:57:54 +0800

    update

---
+++ 6 Class and Style Bindings/main.js
@@ -0,0 +1,14 @@
+/* jshint asi:true */
+new Vue({
+    el: '#app',
+    data: {
+        isP1: true,
+        isP2: false,
+        classObject: {
+            p1: false,
+            p2: true
+        },
+        classA: 'p1',
+        classB: 'bold'
+    }
+})
